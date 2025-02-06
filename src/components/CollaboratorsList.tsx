3e2cba7c1d2fa3e4fa895838af22e3c19397658c
Siby C R <sibycr18@gmail.com>
2025-02-06 18:15:55 +0530

    fixed collaborators double border

--- src/components/CollaboratorsList.tsx
+++ src/components/CollaboratorsList.tsx
@@ -63,7 +63,7 @@ export default function CollaboratorsList() {
 
   return (
     <>
-      <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-4">
+      <div>
         <div className="flex items-center justify-between mb-4">
           <h3 className="font-medium text-gray-900 flex items-center">
             <Users className="w-5 h-5 mr-2 text-gray-500" />
