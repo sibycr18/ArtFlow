f55032bff908455bbc1a00100980930f99cf38ad
Siby C R <sibycr18@gmail.com>
2025-02-07 19:03:49 +0530

    played with animations and caption

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: 'blob 7s infinite',
+        'fade-in': 'fadeIn 1s ease-out forwards',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -50px) scale(1.1)',
+          },
+          '66%': {
+            transform: 'translate(-20px, 20px) scale(0.9)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        },
+        fadeIn: {
+          '0%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0.7',
+          },
+        },
+      },
+      transitionDelay: {
+        '300': '300ms',
+        '600': '600ms',
+        '900': '900ms',
+        '1200': '1200ms',
+      },
+    },
   },
   plugins: [],
 };
